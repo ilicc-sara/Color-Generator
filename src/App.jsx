0ca854bd91ddca5decf5b8e2d49b89bb2bcaa56f
Sara <sarailic431@gmail.com>
2025-07-18 18:41:14 +0200

    ........

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h2>Clor Generator (tints and shades)</h2>
+
+      <form>
+        <label>Set Color</label>
+        <input type="color" />
+        <input type="text" />
+        <button>Submit</button>
+      </form>
+    </>
+  );
 }
 
 export default App;
