ca188b033c7994a3f3d6a44e376e6c1d029996e4
Sara <sarailic431@gmail.com>
2025-07-23 23:20:05 +0200

    ... ... .... .... .. .. .

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 function SingleColor({ index, color }) {
   console.log(color);
   const { hex, weight } = color;
+
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success("Color copied to clipboard");
+      } catch (error) {
+        toast.error("Failed to copy");
+      }
+    } else {
+      toast.error("Clipboard acces not available");
+    }
+  };
   return (
     <article
       className="color"
@@ -10,6 +24,7 @@ function SingleColor({ index, color }) {
         background: `#${hex}`,
         color: `${index > 10 ? "white" : "black"}`,
       }}
+      onClick={() => saveToClipboard()}
     >
       <p className="percent-value"> {weight}% </p>
       <p className="color-value"> #{hex} </p>
